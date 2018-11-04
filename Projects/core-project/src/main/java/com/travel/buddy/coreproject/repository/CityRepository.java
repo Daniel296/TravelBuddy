7f48ef8bef53768cf13f1ce90acf34aa5f570e2b
Andrei HERDES <aherdes@pentalog.com>
2018-11-04 14:36:11 +0200

    Connection to the remote db working Added JpaRepository interfaces for some entities Created a demo endpoint, tested with Postman - working

---
+++ Projects/core-project/src/main/java/com/travel/buddy/coreproject/repository/CityRepository.java
@@ -0,0 +1,14 @@
+package com.travel.buddy.coreproject.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.travel.buddy.coreproject.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Serializable> {
+
+	City findByCityId(Long id);
+}

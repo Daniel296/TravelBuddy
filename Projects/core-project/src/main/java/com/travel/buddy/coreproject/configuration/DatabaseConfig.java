e98645f422f71b7d06b515453fd6e202f8f6a211
Andrei HERDES <aherdes@pentalog.com>
2018-11-04 16:43:20 +0200

    Updated the db config

--- Projects/core-project/src/main/java/com/travel/buddy/coreproject/configuration/DatabaseConfig.java
+++ Projects/core-project/src/main/java/com/travel/buddy/coreproject/configuration/DatabaseConfig.java
@@ -8,11 +8,11 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -21,25 +21,17 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @EnableAutoConfiguration
+@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = { "com.travel.buddy.coreproject.DAOs" })
+@EnableJpaRepositories("com.travel.buddy.coreproject.repository")
 public class DatabaseConfig {
 
 	@Autowired
 	private Environment env;
 
-	@Bean
-	public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
-		LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
-		lcemfb.setJpaVendorAdapter(getJpaVendorAdapter());
-		lcemfb.setDataSource(getDataSource());
-		lcemfb.setPersistenceUnitName("myJpaPersistenceUnit");
-		lcemfb.setPackagesToScan("com.travel.buddy.coreproject.model");
-		lcemfb.setJpaProperties(jpaProperties());
-		return lcemfb;
-	}
+	@Autowired
+	private DataSourceConfiguration dsConfig;
 
 	@Bean
 	public JpaVendorAdapter getJpaVendorAdapter() {
@@ -47,21 +39,21 @@ public class DatabaseConfig {
 		return adapter;
 	}
 
-	@Bean
-	@Primary
-	public DataSource getDataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(env.getProperty("database.driverClassName"));
-		dataSource.setUrl(env.getProperty("database.url"));
-		dataSource.setUsername(env.getProperty("database.username"));
-		dataSource.setPassword(env.getProperty("database.password"));
-		return dataSource;
+	@Bean(name="entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+		lcemfb.setJpaVendorAdapter(getJpaVendorAdapter());
+		lcemfb.setDataSource(dsConfig.getDataSource());
+		lcemfb.setPersistenceUnitName("myJpaPersistenceUnit");
+		lcemfb.setPackagesToScan("com.travel.buddy.coreproject.model");
+		lcemfb.setJpaProperties(jpaProperties());
+		return lcemfb;
 	}
 
-	@Bean
+	@Bean(name="transactionManager")
 	public PlatformTransactionManager txManager() {
 		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(
-				getEntityManagerFactoryBean().getObject());
+				getEntityManagerFactory().getObject());
 		return jpaTransactionManager;
 	}
 
@@ -71,8 +63,22 @@ public class DatabaseConfig {
 		properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
 		properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
 		properties.put("spring.jpa.hibernate.ddl-auto", "update");
-//		properties.put("hibernate.hbm2ddl.auto", "update");
+		//properties.put("hibernate.hbm2ddl.auto", "update");
 		properties.put("hibernate.default_schema", "travel_buddy_database");
 		return properties;
+	}
+
+	@Configuration
+	class DataSourceConfiguration {
+		@Bean
+		@Primary
+		public DataSource getDataSource() {
+			BasicDataSource dataSource = new BasicDataSource();
+			dataSource.setDriverClassName(env.getProperty("database.driverClassName"));
+			dataSource.setUrl(env.getProperty("database.url"));
+			dataSource.setUsername(env.getProperty("database.username"));
+			dataSource.setPassword(env.getProperty("database.password"));
+			return dataSource;
+		}
 	}
 }

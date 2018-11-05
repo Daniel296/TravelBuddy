7d79f3c7799c10dcf947a9187f29bb083123eacf
PENTALOG\echilaboc <c_ecaterina_mihaela96@yahoo.com>
2018-11-05 23:11:32 +0200

    Resolved test errors.

--- Projects/core-project/src/main/test/com/travel/buddy/coreproject/users_recomandations_helper/Test_AttractionRecomandationByUserPreferences.java
+++ Projects/core-project/src/main/test/com/travel/buddy/coreproject/users_recomandations_helper/Test_AttractionRecomandationByUserPreferences.java
@@ -17,38 +17,33 @@ public class Test_AttractionRecomandationByUserPreferences {
     public void TestAttractionRecomandation_GivenUserProfileAndAttractions_ExpectedToReturn_1_Attraction(){
         Faker faker = new Faker();
 
-        //set user interests
-        List<Interest> userInterests = new ArrayList<>();
+        //set user interest
         Interest userInterest = new Interest();
         userInterest.setCanCamping(true);
         userInterest.setCanCycle(true);
-        userInterests.add(userInterest);
+        userInterests.add(userInterest)
 
-        //set attraction 1 interests
-        List<Interest> attractionInterests1 = new ArrayList<>();
+        //set attraction 1 interest
         Interest attractionInterest1 = new Interest();
         attractionInterest1.setCanCamping(true);
         attractionInterest1.setCanSwim(true);
-        attractionInterests1.add(attractionInterest1);
 
-        //set attraction 2 interests
-        List<Interest> attractionInterests2 = new ArrayList<>();
+        //set attraction 2 interest
         Interest attractionInterest2 = new Interest();
         attractionInterest2.setCold(true);
         attractionInterest2.setCanCycle(true);
-        attractionInterests2.add(attractionInterest2);
 
 
         List<Attraction> attractions = new ArrayList<>();
 
         //attraction1
         Attraction attraction1 = new Attraction();
-        attraction1.setInterests(attractionInterests1);
+        attraction1.setInterest(attractionInterest1);
 
 
         //attraction2
         Attraction attraction2 = new Attraction();
-        attraction2.setInterests(attractionInterests2);
+        attraction2.setInterest(attractionInterest2);
 
 
         // attraction list
@@ -59,7 +54,7 @@ public class Test_AttractionRecomandationByUserPreferences {
         //our user
         String fName = faker.name().firstName();
         UserProfile userProfile = new UserProfile(fName, null, null, null, null, null, null, null, null);
-        userProfile.setInterests(userInterests);
+        userProfile.setInterest(userInterest);
 
         AttractionRecomandationByUserPreferencesHelperImpl helper = new AttractionRecomandationByUserPreferencesHelperImpl(userProfile);
         helper.setAllAttractions(attractions);

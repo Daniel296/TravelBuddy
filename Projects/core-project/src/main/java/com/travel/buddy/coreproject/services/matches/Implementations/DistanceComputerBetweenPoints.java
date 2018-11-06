f30eaacd81e7ef76e941e1d6e078a59a9c687aa6
Daniel <daniel.oana26@gmail.com>
2018-11-06 22:29:01 +0200

    New tests added for calculating distance between attractions

--- Projects/core-project/src/main/java/com/travel/buddy/coreproject/services/matches/Implementations/DistanceComputerBetweenPoints.java
+++ Projects/core-project/src/main/java/com/travel/buddy/coreproject/services/matches/Implementations/DistanceComputerBetweenPoints.java
@@ -1,5 +1,7 @@
 package com.travel.buddy.coreproject.services.matches.Implementations;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,31 +18,31 @@ public class DistanceComputerBetweenPoints {
             distance += calculateDistance(points.get(i-1).getLatitude(), points.get(i-1).getLongitude(), points.get(i).getLatitude(), points.get(i).getLongitude(), unit);
         }
 
-        return distance;
+        return (new BigDecimal(distance)).setScale(3, RoundingMode.HALF_UP).doubleValue();
     }
 
     private static double calculateDistance(double lat1, double lng1, double lat2, double lng2, String unit) {
         /* calculates the distance between two points (given the latitude/longitude of those points) */
         double theta = lng1 - lng2;
-        double distance = (Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))) + (Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) * Math.sin(deg2rad(theta)));
+        double distance = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
         distance = Math.acos(distance);
         distance = rad2deg(distance);
         distance = distance * 60 * 1.1515;
 
         if(unit.equals("KM")) {
-            return distance * 0.8684;
+            return distance * 1.609344;
         }
 
-        return distance;    // in Miles
+        return (new BigDecimal(distance)).setScale(2, RoundingMode.HALF_UP).doubleValue();    // in Miles or Kilometers
     }
 
     private static double deg2rad(double deg) {
         /* converts decimal degrees to radians */
-        return deg * (Math.PI / 180);
+        return (deg * Math.PI / 180.0);
     }
 
     private static double rad2deg(double rad) {
         /* converts radians to decimal degrees */
-        return rad * (Math.PI / 180);
+        return (rad * 180 / Math.PI);
     }
 }
---
+++ Projects/core-project/src/main/test/com/travel/buddy/coreproject/users_matches_helper/Test_CalculatingDistance.java
@@ -0,0 +1,69 @@
+package com.travel.buddy.coreproject.users_matches_helper;
+
+import com.travel.buddy.coreproject.model.Attraction;
+import com.travel.buddy.coreproject.services.matches.Implementations.DistanceComputerBetweenAttractions;
+import com.travel.buddy.coreproject.services.matches.Implementations.DistanceComputerBetweenPoints;
+import com.travel.buddy.coreproject.services.matches.Implementations.Point;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_CalculatingDistance {
+
+    @Test
+    public void Test_DistanceTwoPoints() {
+        Point point1 = new Point(48.64703, -122.26324);
+        Point point2 = new Point(48.6721, -122.265);
+
+        double distanceBetweenPoints = DistanceComputerBetweenPoints.getDistance("KM", point1, point2);
+
+        Assert.assertEquals(2.791, distanceBetweenPoints, 1e-15);
+    }
+
+    @Test
+    public void Test_DistanceBetweenAListOfPoints() {
+        List<Point> points = new ArrayList<>();
+
+        Point point1 = new Point(48.64703, -122.26324);
+        Point point2 = new Point(48.6721, -122.265);
+        Point point3 = new Point(48.4234, -122.34);
+
+        points.add(point1);
+        points.add(point2);
+        points.add(point3);
+
+        double distanceBetweenPoints = DistanceComputerBetweenPoints.getDistance("KM", points);
+
+        Assert.assertEquals(DistanceComputerBetweenPoints.getDistance("KM", point1, point2) + DistanceComputerBetweenPoints.getDistance("KM", point2, point3), DistanceComputerBetweenPoints.getDistance("KM", points), distanceBetweenPoints);
+    }
+
+    @Test
+    public void Test_DistanceBetweenTwoAttractions() {
+        Attraction attraction1 = new Attraction(0, null, 48.64703, -122.26324, null, null, null);
+        Attraction attraction2 = new Attraction(0, null, 48.6721, -122.265, null, null, null);
+
+        double distanceBetweenAttractions = DistanceComputerBetweenAttractions.getDistance("KM", attraction1, attraction2);
+
+        Assert.assertEquals(2.791, distanceBetweenAttractions, 1e-15);
+    }
+
+    @Test
+    public void Test_DistanceBetweenAListOfAttractions() {
+        List<Attraction> attractions = new ArrayList<>();
+
+        Attraction attraction1 = new Attraction(0, null, 48.64703, -122.26324, null, null, null);
+        Attraction attraction2 = new Attraction(0, null, 48.6721, -122.265, null, null, null);
+        Attraction attraction3 = new Attraction(0, null, 48.4234, -122.34, null, null, null);
+
+        attractions.add(attraction1);
+        attractions.add(attraction2);
+        attractions.add(attraction3);
+
+        double distanceBetweenPoints = DistanceComputerBetweenAttractions.getDistance("KM", attractions);
+
+        Assert.assertEquals(DistanceComputerBetweenAttractions.getDistance("KM", attraction1, attraction2) + DistanceComputerBetweenAttractions.getDistance("KM", attraction2, attraction3), DistanceComputerBetweenAttractions.getDistance("KM", attractions), distanceBetweenPoints);
+
+    }
+}
